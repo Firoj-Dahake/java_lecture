5777693f74e4cbe4e77c1019a24fcf6660629ca9
Firoj Dahake <141325720+Firoj-Dahake@users.noreply.github.com>
2023-08-30 14:01:48 +0530

    array.java

--- src/array.java
+++ src/array.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 public class array {
     public static void main(String[] args) {
- //       int [] marks=new int[5]; //declaration+memori-allocation //array is object
-        /*Scanner sc=new Scanner(System.in);
+        int [] marks=new int[5]; //declaration+memori-allocation //array is object
+        Scanner sc=new Scanner(System.in);
         int [] marks; //declarization
-        marks=new int[5]; *///memori-allocation
-        /*marks[0]=100;
+        marks=new int[5]; //memori-allocation
+        marks[0]=100;
         marks[1]=85;
         marks[2]=96;
         marks[3]=83;
-        marks[4]=79;*/
+        marks[4]=79;
         int [] marks={78,87,95,70,99};//Decleration+Memory-Allocation+Initialization
         System.out.println(marks[3]);
     }
